a80421a5eea958910fdfb8fb2aa7e0cab3a71862
yassineaitalla <m.aitallayassine@gmail.com>
2024-01-03 11:26:40 +0100

    1er commit avant articles

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'app_blog')]
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    #[Route('/', name: 'home')]
+    public function home():Response
+    {
+
+    return $this->render('blog/home.html.twig',[
+    'title'=>"Page Accueil",
+    'age'=>27
+    ] );
+    }
+
+}
